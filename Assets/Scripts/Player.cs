d3af4ca49cfe2e71e0b5a1d25f2a58703a69ee91
therealfra <francesco_volpato@studenti.naba.it>
2020-11-26 15:33:57 +0100

    controllo player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D MyRigidBody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MyRigidBody.velocity = Vector2.up * speed;
+        }
+    }
+}
