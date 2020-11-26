f612a6897e8cf70860480526c492dd0c8a02c067
therealfra <francesco_volpato@studenti.naba.it>
2020-11-26 17:01:38 +0100

    game over & replay

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     public float speed;
     Rigidbody2D MyRigidBody;
 
+    //reference score e replay button
     public Score scoreText;
+    public GameObject ReplayBtn;
 
     void Start()
     {
+        Time.timeScale = 1;
         MyRigidBody = GetComponent<Rigidbody2D>();
     }
 
@@ -29,5 +33,20 @@ public class Player : MonoBehaviour
             print("Score Up");
             scoreText.ScoreUp();
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Pipe"))
+        {
+            //game over
+            Time.timeScale = 0;
+            ReplayBtn.SetActive(true);
+        }
+    }
+
+    public void ReplayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
